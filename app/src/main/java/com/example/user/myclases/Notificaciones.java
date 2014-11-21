8058e275fc4f8514947e9c01806eeadbb3e7cb27
gonzaloriquelme <gjavierf@gmail.com>
2014-11-20 22:13:09 -0300

    MartinBarrene

--- app/src/main/java/com/example/user/myclases/Notificaciones.java
+++ app/src/main/java/com/example/user/myclases/Notificaciones.java
@@ -1,17 +1,42 @@
 package com.example.user.myclases;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Notificaciones extends Activity {
+    NotificationManager nm;
+    private static final int ID_NOTIFICACION_PERSONAL = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notificaciones);
+        Button btn_lanzar = (Button) findViewById(R.id.btn_notificacion);
+
+        nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        btn_lanzar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Notification notification = new Notification(R.drawable.ic_launcher, "Alumno 1", System.currentTimeMillis());
+
+                PendingIntent intencionpendiente = PendingIntent.getActivity(getApplicationContext(), 0, new Intent(getApplicationContext(), Contenido.class), 0);
+
+                notification.setLatestEventInfo(getApplicationContext(), "Notificacion", "Alumno 1", intencionpendiente);
+
+                nm.notify(ID_NOTIFICACION_PERSONAL, notification);
+            }
+        });
     }
 
 
